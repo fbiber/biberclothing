d5ee140d87449959c8a7734c7fae0ca2a19d3240
fbiber <fbiber@gmail.com>
2020-04-15 13:02:38 +0100

    Routing initial developments added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,7 @@ import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 const HomePage = () => (
+
     <div className='homepage'>
         <Directory />
     </div>
