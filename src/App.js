6ff2c1310715fffcea05e41903e9972920eab944
fbiber <fbiber@gmail.com>
2020-04-17 11:58:33 +0100

    Redux introduction, changing the user state to redux state inside header component

--- src/App.js
+++ src/App.js
@@ -6,37 +6,26 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserFromDocument } from './firebase/firebase.utils';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user-actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async authUser => {
       if(authUser) {
         const userRef = await createUserFromDocument(authUser);
 
         userRef.onSnapshot(snapshot => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapshot.id,
-              ...snapshot.data()
-              }
-            }
-          );
+          setCurrentUser({
+            id: snapshot.id,
+          ...snapshot.data()
+          });
         });
       } else {
-        this.setState({
-          currentUser: authUser
-        });
+        setCurrentUser(authUser);
       }
 
       console.log(authUser);
@@ -51,17 +40,19 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Header currentUser={this.state.currentUser}></Header>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/shop" component={ShopPage} />
-            <Route path="/signin" component={SignInPage} />
-          </Switch>
-        </BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInPage} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
